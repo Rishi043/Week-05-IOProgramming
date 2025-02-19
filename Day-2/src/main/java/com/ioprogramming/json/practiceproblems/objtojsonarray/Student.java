0690137bbc73900fb2855fcf4bb36e264c3f385b
Rishi Thakur <rishithakur9131@gmail.com>
2025-02-19 21:20:24 +0530

    [Rishi] Add. Solution of Convert a list into a JSON array

---
+++ Day-2/src/main/java/com/ioprogramming/json/practiceproblems/objtojsonarray/Student.java
@@ -0,0 +1,30 @@
+package com.ioprogramming.json.practiceproblems.objtojsonarray;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Student {
+    private String name;
+    private int age;
+    private String email;
+
+    // Constructor
+    public Student(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters (Required for Jackson)
+    public String getName()
+    {
+        return name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+}
